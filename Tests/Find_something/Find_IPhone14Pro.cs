fb9a411f52bf2211923a58f50f83cd8a0f7ab61b
Peyzzz <pejosek@gmail.com>
2023-03-17 10:30:30 +0100

    Added gitignore

--- Tests/Find_something/Find_IPhone14Pro.cs
+++ Tests/Find_something/Find_IPhone14Pro.cs
@@ -1,4 +1,5 @@
-﻿using PepperQA.Pages;
+﻿using Microsoft.Playwright;
+using PepperQA.Pages;
 using PepperQA.Settings;
 using PepperQA.Templates;
 
@@ -10,11 +11,22 @@ namespace PepperQA.Tests.Find_something
         [TestCase ("Find IPhone 14 pro")]
         public async Task GotoPepper_Find_IPhone14Pro(string name)
         {
+            Playwright.Selectors.SetTestIdAttribute("data-t");
+
             var page = await Context.NewPageAsync();
             var homePage = new HomePage(page);
             await page.GotoAsync(TestSettings.EnvUrl);
 
+            await page.GetByRole(AriaRole.Button, new() { Name = "Kontynuuj bez akceptacji" }).ClickAsync();
 
+            await page.GetByRole(AriaRole.Link, new() { Name = "Nowe" }).ClickAsync();
+
+            int i = 0;
+
+            while (i < 3)
+            {
+                await page.GotoAsync($"https://www.pepper.pl/nowe?page={i}");
+            }
         }
     }
 }
